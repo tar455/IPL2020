6aedc7822f2a3ebb2438926da4eee25146dfa7df
Tarique <tariqueansari455@gmail.com>
2020-09-21 22:05:52 +0530

    Points Table

--- Data.R
+++ Data.R
@@ -1,2 +1,10 @@
+
+install.packages("rvest")
+library(rvest)
+
+url <- "https://www.iplt20.com/match/2020/01?tab=scorecard"
+website <- read_html(url)
+datain_web <-html_table(website,header<-NA,trim<- TRUE,fill <-FALSE,dec=".")
+print(datain_web)
 print("This project perform  Analysis on IPL 2020")
 print("Let's ready for that")
