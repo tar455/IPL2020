c158599e980d4bf840095feb43e46feb0d605de5
Tarique <tariqueansari455@gmail.com>
2020-10-22 12:19:24 +0530

    Ipl2020

--- Data.R
+++ Data.R
@@ -2,9 +2,10 @@
 install.packages("rvest")
 library(rvest)
 
-url <- "https://www.iplt20.com/match/2020/01?tab=scorecard"
+url <- "https://www.iplt20.com/stats/2020/most-runs"
 website <- read_html(url)
 datain_web <-html_table(website,header<-NA,trim<- TRUE,fill <-FALSE,dec=".")
-print(datain_web)
+print(datain_web[3])
 print("This project perform  Analysis on IPL 2020")
 print("Let's ready for that")
+
